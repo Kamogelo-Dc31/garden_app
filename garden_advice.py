67f06bc39f28ca3b96c689b4c70f4d7e199f1113
kamogelo <kamokabza156@gmail>
2025-06-06 13:22:41 +0200

    Replace hardcoded values with input() for user interaction

--- garden_advice.py
+++ garden_advice.py
@@ -1,6 +1,7 @@
 # Hardcoded values for the season and plant type
-season = "summer"  # TODO: Replace with input() to allow user interaction.
-plant_type = "flower"  # TODO: Replace with input() to allow user interaction.
+season = input("Enter the season (e.g., summer, winter): ").lower()
+plant_type = input("Enter the plant type (e.g., flower, vegetable): ").lower()
+
 
 # Variable to hold gardening advice
 advice = "this is advice"
